bd5f018a814665963511afea1c74541900d5946c
hiba <hibajamalelarab@gmail.com>
2023-06-16 23:38:35 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Prints the alphabetics
  * Return: Always (Success)
  *
  */
+
 int main(void)
+
 {
+
         char c;
 
         for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
-        return (0);
+ 	return (0);
+
 }
