5b580aa4f902b66c7c1d20d0b38eec0f4960d81e
hiba <hibajamalelarab@gmail.com>
2023-06-16 23:43:04 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,11 @@
 int main(void)
 
 {
+	char c;
 
-        char c;
-
-        for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
- 	return (0);
+	return (0);
 
 }
