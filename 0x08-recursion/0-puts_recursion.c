8c967a64eaf505b0e33fbc5e5262b87373ddad07
hiibaaa <hibajamalelarab@gmail.com>
2023-10-26 16:51:48 -0700

    Speed fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,37 @@
-#include "main.h"
-
+lude "main.h"
 /**
- * _puts_recursion - function that prints a string, followed by a new line.
- * @s: string will be printed
- */
+ *  * _puts_recursion - function like puts();
+ *   * @s: input
+ *    * Return: Always 0 (Success)
+ *     */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
-	{	
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
+		if (*s)
+				{
+							_putchar(*s);
+									_puts_recursion(s + 1);
+										}
+			else
+					{
+								_putchar('\n');
+									}
+}#include "main.h"
+/**
+ *  * _puts_recursion - function like puts();
+ *   * @s: input
+ *    * Return: Always 0 (Success)
+ *     */
 
+void _puts_recursion(char *s)
+{
+		if (*s)
+				{
+							_putchar(*s);
+									_puts_recursion(s + 1);
+										}
+			else
+					{
+								_putchar('\n');
+									}
 }
