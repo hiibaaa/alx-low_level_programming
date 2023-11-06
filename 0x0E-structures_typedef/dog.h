758738d797d76c7a6139ad044221cbedd6953e4b
hiibaaa <hibajamalelarab@gmail.com>
2023-11-06 08:25:33 -0800

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog_h
+#define dog_h
 
 /**
  * A struct DOG
@@ -11,9 +11,14 @@
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
