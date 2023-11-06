7f37bd690535c7137f5270e8e2c3e312ef1b2468
hiibaaa <hibajamalelarab@gmail.com>
2023-11-06 06:43:55 -0800

    PREPROCESSOR

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name of this source file
+ *
+ * return 0 ;
+ *
+ */
+
+int main(void)
+{
+	print("%s\n" , __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACRO.H
+#define _FUNCTION_LIKE_MACRO.H
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /*_FUNCTION_LIKE_MACRO.H */
