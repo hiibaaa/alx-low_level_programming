de8cde79f8900413d6e04cc685f49e80c94f8595
hiibaaa <hibajamalelarab@gmail.com>
2023-11-16 13:54:59 -0800

    singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
-#include "list.h"
+#include "lists.h"
 
 /**
-* a function that prints all the elements of a list_t list
-*@h: Pointer to the head of the list.
-*
-*Return: number of nodes.
-*/
+ * print_list - Prints all the elements of a linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 
 size_t print_list(const list_t *h)
 {
